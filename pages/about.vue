405c2bcfe802ee1b743bcd6c1e8cb99db845c969
xrudoga <defcw58@gmail.com>
2020-04-12 21:00:50 +0800

    frontend: Admin App

---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+    <input v-model="name" />
+    <button @click="onclick">Click me</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        name: 'about me im awesome'
+      }
+    },
+    methods: {
+      onclick() {
+        alert(this.name)
+      }
+    }
+  }
+</script>
